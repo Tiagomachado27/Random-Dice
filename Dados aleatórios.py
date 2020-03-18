22c5c1bbd3c6aeb0ae3b6eb06a59700eb7ec95ac
Tiagomachado27 <43068863+Tiagomachado27@users.noreply.github.com>
2020-03-18 03:29:39 +0000

    Update Dados aleatórios.py

--- Dados aleatórios.py
+++ Dados aleatórios.py
@@ -3,5 +3,16 @@ import random
 dice_nr = input("Quantos dados quer lançar? ")
 dice_shape = input("Quantos lados têm os dados? ")
 
+repeat = ("")
+
+
 for x in range(int(dice_nr)):
     print(random.randint(1, int(dice_shape)))
+while True :
+    repeat = input("Deseja lançar outra vez?\n"  "(Pressione 's' para lançar outra vez)")
+    if repeat == "s":
+        for x in range(int(dice_nr)):
+            print(random.randint(1, int(dice_shape)))
+    else:
+        close = input("Pressione Enter para fechar o programa.")
+        break
